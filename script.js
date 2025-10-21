bb5c7e16d72d552192cd79728d1b95455a266e96
xionXinYunGitHub <125775287+xionXinYunGitHub@users.noreply.github.com>
2025-10-21 22:45:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+(function(window){
 // 保存原生XMLHttpRequest
 const OriginalXHR = window.XMLHttpRequest;
 
@@ -5,9 +6,10 @@ const OriginalXHR = window.XMLHttpRequest;
 class MyXHR extends OriginalXHR {
   constructor() {
     super();
-    alert("工具初始化...");
+    alert("初始化XHR代理");
     // 拦截请求发送前（open之后、send之前）
     this.addEventListener('loadstart', (e) => {
+      alert("发送请求:"+this.url);
       console.log('【拦截XHR请求】', {
         method: this.method, // 请求方法（GET/POST）
         url: this.url,       // 请求地址
@@ -21,6 +23,7 @@ class MyXHR extends OriginalXHR {
         status: this.status, // 响应状态码
         response: this.responseText // 响应内容
       });*/
+      alert("收到请求..."+this.url);
       if (this.url.includes('studyservice-api.zhihuishu.com/gateway/t/v1/popupAnswer/lessonPopupExam')) {
       alert('url:'+this.url);
       if(this.status === 200){
@@ -54,3 +57,4 @@ class MyXHR extends OriginalXHR {
 
 // 替换全局XMLHttpRequest，让所有请求使用自定义XHR
 window.XMLHttpRequest = MyXHR;
+})(window);
